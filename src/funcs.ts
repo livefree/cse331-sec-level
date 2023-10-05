1d49f7cd210289dd542435fa2a2ca3b3662b2892
Kevin Zatloukal <kevinz@cs.washington.edu>
2023-10-04 21:15:08 -0700

    Starter code for sec-levels

---
+++ src/funcs.ts
@@ -0,0 +1,18 @@
+/** Calculates (incorrectly) the value (n-1)^2. */
+export const quadratic1 = (n: number): number => {
+  return -1;  // TODO: change this
+}
+
+/** Calculates (incorrectly) the value (n-1)^2. */
+export const quadratic2 = (n: number): number => {
+  return -1;  // TODO: change this
+}
+
+
+/** Calculates (incorrectly) the value |x|.  */
+export const abs_value = (x: number): number => {
+  return 0;  // TODO: change this to use an "if" with returns of x and -x
+}
+
+
+// TODO: add the definition of "half" here
